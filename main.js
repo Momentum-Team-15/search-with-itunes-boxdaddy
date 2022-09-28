772f00742ace6ddb51f2c3beeba9af208065b3ce
Chris <cg.glatz@gmail.com>
2022-09-28 13:21:22 -0400

    search is displaying to page

--- main.js
+++ main.js
@@ -1,37 +1,66 @@
 
-let input = document.getElementById("artist-search")
-let form = document.querySelector('form')
-let searchResults = document.getElementById('search-results')
+const search = document.createElement('div');
 
-form.addEventListener('submit', (event) => {
-    let search = input.value;
-    let url = 'https://proxy-itunes-api.glitch.me/search?musicArtist'
-    event.preventDefault();
-    
-    
+let input = document.createElement('input');
+input.type = 'text';
+search.appendChild(input);
+
+let searchBaseUrl = 'https://proxy-itunes-api.glitch.me/search?term=';
+
+let searchForm = document.querySelector('#search-form')
+    searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log('submitted!')
+    let searchBox = document.querySelector('#search-box')
+    let searchUrl = `${searchBaseUrl}${searchBox.value}`
+    console.log('search url', searchUrl)
+    getSearchResults(searchUrl)
+})
+ function getSearchResults(url) {
+
     fetch(url)
         .then(function(response){
             return response.json()
         })
-
-        .then(function(music) {
-            buildResults(music.results);
+        .then(data => {
+            let songs = data.results;
+            showResults(songs);
+        }).catch(error => {
+            console.log(error);
+            alert(`Your request failed, for the reason: ${error}`);
         })
+        
+        
+        let resultsDiv = document.querySelector('#results');
+        console.log('results div', resultsDiv);
+        
+        function showResults(songArray) {
+            resultsDiv.innerHTML = ('')
+            console.log(songArray);
+            if (songArray.length === 0) {
+                resultsDiv.innerText = `Uh oh! I couldn't find music to match your search. Please try again`;
+            } else {
+            for (let song of songArray){
+                let recordDiv = document.createElement('div');
+                recordDiv.classList.add('record');
+        
+                let imageDiv = document.createElement('img');
+                imageDiv.classList.add('image');
+                imageDiv.src = song.artworkUrl100;
+        
+                let titleDiv = document.createElement('div');
+                titleDiv.classList.add('div');
+                titleDiv.innerText = `${song.trackName}`;
+        
+                let artistDiv = document.createElement('div');
+                artistDiv.classList.add('div');
+                artistDiv.innerText = `${song.artistName}`;
+        
+                resultsDiv.appendChild(recordDiv);
+                recordDiv.appendChild(imageDiv);
+                recordDiv.appendChild(titleDiv);
+                recordDiv.appendChild(artistDiv);
+        
 
-})
 
-function buildResults(itunesArray){
-    for (let itunes of itunesArray) {
-        let resultsDiv= document.createElement("div");
-        let picture = document.createElement("img");
-        let artistName = document.createElement("h6");
-        let trackName = document.createElement("h6");
-        let audio = document.getElementById("audio-player")
-        resultsDiv.classList.add("results")
-
-    }
-}
-
-
-
-
+            }}}}
